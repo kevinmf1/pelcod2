56da7b2e1ffe89287b0592fa083595fef3a1f207
Kevin Malik Fajar <97342935+kevinmf1@users.noreply.github.com>
2023-09-02 10:59:30 +0700

    Add files via upload
    upload file

---
+++ latihan1.py
@@ -0,0 +1,49 @@
+nama_saya = str(input("masukan nama kamu = "))
+print(nama_saya, "bertipe",  type(nama_saya))
+
+# kode ini buat memasukan umur
+umur = int(input("masukan umur kamu = "))
+print(umur, "bertipe",  type(umur))
+
+IPK = float(input("masukan ipk kamu = "))
+print(IPK, "bertipe",  type(IPK))
+
+menikah = bool(input("sudah menikah? = "))
+print("menikah?",menikah, "bertipe",  type(menikah))
+
+# matkul_diambil = ["alpro", "sig"]
+# print("Matkul : ",matkul_diambil, "bertipe",  type(matkul_diambil))
+
+# matkul_diambil2 = ("web","pemvis")
+# print("Matkul Lain: ",matkul_diambil2, "bertipe",  type(matkul_diambil2))
+
+# matkul_diambil3 = {"Alpro", "ALpro", "PemWeb", "Data Science", "Data Science"}
+# print("Matkul Lain-Lain: ",matkul_diambil3, "bertipe",  type(matkul_diambil3))
+
+
+
+
+
+
+# print()
+# print()
+
+# print(17 / 3)
+# print(17 // 3)
+# print(17 % 3)
+
+
+
+# nama_universitas = "Universitas Trunojoyo"
+# print(nama_universitas)
+
+# print()
+# print()
+
+# nama_universitas = str(input("masukan nama universitas = "))
+# print(nama_universitas)
+
+# angka1 = int(input("masukan angka 1 = "))
+# angka2 = int(input("masukan angka 2 = "))
+# print(angka1 + angka2)
+# ctrl + /
